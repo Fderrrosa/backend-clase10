3548377d2e6aa4a13270247ea509f4a7c33c689d
Francesco de rosa <fderosa067@gmail.com>
2022-11-17 20:51:20 -0300

    sigo sin encontrar el error

--- public/js/index.js
+++
@@ -1,5 +0,0 @@
-const socket = io();
-
-socket.on('mi mensaje', data => {
-    alert(data)
-})
--- server.js
+++ server.js
@@ -3,65 +3,73 @@ const express = require("express");
 
 //se carga el modulo handlebars
 const handlebars = require("express-handlebars");
-
 //se carga modulo socket.io
 const { Server } = require('socket.io');
 
+const path = require("path");
+const viewsFolder = path.join(__dirname, "/src/views")
+const Contenedor = require("./src/container/contenedor")
+const productosService = new Contenedor("productos.txt");
 
-
-//se crea el servidor
+//servidor de express
 const app = express();
+const server = app.listen(8080,()=>console.log("server listening on port 8080"));
+
 
 //se inicializa la funcion
 //const httpServer = new HttpServer(app);
 //const io = new IOServer(httpServer);
 //se carga modulo Http?
 //const { Server: HttpServer } = require('http');
-
-
-
-//se levanta el servidor
-const server = app.listen(8080,()=>console.log("server listening on port 8080"));
-
 //httpServer.listen(3000, () => console.log('SERVER ON'))
+
 
 
 //ajustes
 app.use(express.json());
-app.use(express.static(__dirname+"/public"));
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.static(__dirname+"./src/public"));
+app.use(express.urlencoded({extended:true})); 
 
 
-const path = require("path");
-const viewsFolder = path.join(__dirname, "views")
-const Contenedor = require("./src/container/contenedor")
-const productosService = new Contenedor("./products/productos.txt");
 
 
-//socket del lado del backend
-const io = new Server(server);
-
-
-//inicializar motor de plantillas
-app.engine("handlebars", handlebars.engine());
-//donde estas las vistas del proyecto
+//vamos a inicializar nuestro motor de plantillas.
+app.engine("handlebars",handlebars.engine());
+//donde tengo las vistas en mi proyecto
 app.set("views", viewsFolder);
-//que motor de plantillas vamos a utilizar 
+//que motor de plantillas voy a utilizar
 app.set("view engine", "handlebars");
 
 
 
-//rutas 
-app.get("/", (req, res)=>{
-    res.render("home")
+//socket del lado del backend
+const io = new Server(server);
+io.on("connection", async(socket)=>{
+    console.log("nuevo cliente conectado");
+
+
+    //cada vez que el socket se conecte le enviamos los productos
+    socket.emit("productsArray", await productosService.getAll());
+
+    //recibir el producto
+    socket.on("newProduct", async(data)=>{
+        //data es el producto que recibo del formulario
+        await productosService.save(data);
+
+        //enviar todos los productos actualizados
+        io.sockets.emit("productsArray", await productosService.getAll());
+    })
 })
 
 
 
 
-//app.get("/", (req, res) =>{
-  //  res.render("home")
-//})
+//rutas 
+app.get("/",(req, res)=>{
+    res.render("home")
+})
+
+
 
 app.get("/productos", async(req, res)=>{
     const productos = await productosService.getAll();
@@ -81,13 +89,4 @@ app.post("/productos",async(req, res)=>{
 
 
 
-app.get("/contact", (req, res)=>{
-    res.render("contacto")
-})
 
-
-
-io.on('connection', (socket) => {
-    console.log('Usuario conectado')
-    //socket.emit('mi mensaje', 'este es mi mensaje desde el servidor')
-})
